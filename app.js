80a4f554894c970c3d0c28870bb248f36cbb00ef
Hoon Jeong <yong08211@gmail.com>
2024-07-16 22:06:03 -0400

    add session store

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const passport = require("passport");
 const bcrypt = require("bcryptjs");
 const LocalStrategy = require("passport-local").Strategy;
 const { body, validationResult } = require("express-validator");
-require("dotenv").config();
-
+const MongoStore = require("connect-mongo");
 const User = require("./models/user");
 const mongoose = require("mongoose");
+require("dotenv").config();
+
 mongoose.set("strictQuery", false);
 const mongoDB = process.env.DB_STRING;
 
@@ -18,21 +19,28 @@ main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
-//TODO: make session store
+// const connection = mongoose.createConnection(process.env.DB_STRING);
+const sessionStore = MongoStore.create({
+  mongoUrl: process.env.SESSION_STORE,
+});
 var app = express();
 app.set("view engine", "ejs");
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
   })
 );
 app.use(passport.session());
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
@@ -43,7 +51,7 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/sign-up", (req, res) => res.render("sign-up",{errors:null}));
+app.get("/sign-up", (req, res) => res.render("sign-up", { errors: null }));
 
 app.get("/log-out", (req, res, next) => {
   req.logout((err) => {
@@ -56,21 +64,23 @@ app.get("/log-out", (req, res, next) => {
 
 app.post(
   "/sign-up",
-  body("password").isLength({ min: 5 }).withMessage("Password must be at least 5 characters"),
-  body("confirmpwd").custom((value, { req }) => {
-    return value === req.body.password;
-  }).withMessage("Password does not match with confirmation"),
+  body("password")
+    .isLength({ min: 5 })
+    .withMessage("Password must be at least 5 characters"),
+  body("confirmpwd")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Password does not match with confirmation"),
   async (req, res, next) => {
     const errors = validationResult(req);
 
-    if (!errors.isEmpty()){
+    if (!errors.isEmpty()) {
       res.render("sign-up", {
         errors: errors.array(),
       });
       return;
-
     }
-
 
     bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
       if (err) {
